74a8352f7814f160106f138860e116c506a5900e
muaad <muadh24@gmail.com>
2015-09-11 15:38:01 +0300

    rails_apps_composer: set up database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/questions'
+
   root to: 'visitors#index'
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
